14440db110169364f5d015bcab7c396adcba2fff
murali0101 <muralimv0101@gmail.com>
2022-06-06 10:31:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const getData = async () => {
     "https://www.google.com/search?q=react+jobs&ei=4duYYpLRJYq94-EPqfK90AE&uact=5&oq=react+jobs&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMggIABCABBDJAzIFCAAQkgMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgQIABBHOgUIABCRAjoUCC4QgAQQsQMQgwEQxwEQ0QMQ1AI6CAguEIAEELEDOgUILhCABDoLCAAQgAQQsQMQgwE6BAgAEEM6DgguEIAEELEDEMcBEKMCOgsILhDHARCvARCRAjoHCAAQyQMQQzoOCC4QgAQQsQMQxwEQ0QM6CAgAEIAEELEDOggILhCABBDUAjoLCC4QgAQQsQMQ1AI6DQgAELEDEIMBEMkDEEM6CAgAELEDEIMBOgoIABCxAxCDARANOgQIABANOgcIABDJAxANSgQIQRgASgQIRhgAUJseWIgzYOczaANwAngAgAGmAYgB5AqSAQQwLjExmAEAoAEBsAEAyAEIwAEB&sclient=gws-wiz&ibp=htl;jobs&sa=X&ved=2ahUKEwi546uOj4_4AhXh6zgGHQlPDQYQutcGKAF6BAgHEAY#htivrt=jobs&htidocid=DNXJc0LPw_4AAAAAAAAAAA%3D%3D&fpstate=tldetail"
   );
 
-  const grabDetalis = await page.evaluate(() => {
+  const Detalis = await page.evaluate(() => {
     var res = [];
     const data = document.querySelectorAll(
       ".iFjolb.gws-plugins-horizon-jobs__li-ed"
@@ -30,15 +30,18 @@ const getData = async () => {
     });
     return res;
   });
-  console.log(grabDetalis);
-  await browser.close();
-};
-app.get("/", (req, res) => {
-  try {
-    return res.status(200).send("grabDetalis");
-  } catch (error) {
-    console.log(error);
-  }
-});
 
-getData();
+    await browser.close();
+    return Detalis
+};
+
+
+
+app.get("/", async (req, res) => {
+    try {
+      const data = await getData()
+      return res.status(200).send(data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
